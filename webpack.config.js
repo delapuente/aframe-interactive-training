bfad20c388d922ad575cd6d41534c2afe0eaead7
Salvador de la Puente González <salva@unoyunodiez.com>
2017-06-16 18:37:20 +0200

    Step 1 done

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+
+module.exports = {
+  entry: {
+    step1: './js/step1/index.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: [path.resolve(__dirname), path.resolve(__dirname, 'assets')],
+    watchContentBase: true
+  }
+};
